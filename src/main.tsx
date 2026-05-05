9d70e91b8e7ab8b43e3d059c4da85b0557d46e90
leohkz <30283225+leohkz@users.noreply.github.com>
2026-05-06 05:57:57 +0800

    feat: import dock.css for macOS dock animation

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@fontsource-variable/plus-jakarta-sans/index.css'
 import './index.css'
+import './dock.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
